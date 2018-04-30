db02e140ed3593b99f0836d863d1c256123b0084
Pablo Lopez Domowicz <pablolopezdomowicz@Pablos-MacBook-Pro.local>
2018-04-29 23:55:19 -0500

    added gorilla mux and aace templates

--- main.go
+++ main.go
@@ -4,18 +4,24 @@ import (
 	"database/sql"
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
-	"html/template"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 
+	gmux "github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/urfave/negroni"
+	"github.com/yosssi/ace"
 )
 
 type Page struct {
-	Name     string
-	DBStatus bool
+	Books []Book
+}
+type Book struct {
+	PK             int
+	Title          string
+	Author         string
+	Classification string
 }
 
 type SearchResult struct {
@@ -25,23 +31,40 @@ type SearchResult struct {
 	ID     string `xml:"owi,attr"`
 }
 
+var db *sql.DB
+
+func verifyDatabase(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	if err := db.Ping(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	next(w, r)
+}
+
 func main() {
-	templates := template.Must(template.ParseFiles("templates/index.html"))
+	db, _ = sql.Open("sqlite3", "dev.db")
+	mux := gmux.NewRouter()
 
-	db, _ := sql.Open("sqlite3", "dev.db")
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		p := Page{Name: "Pablo"}
-		if name := r.FormValue("name"); name != "" {
-			p.Name = name
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template, err := ace.Load("templates/index", "", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-		p.DBStatus = db.Ping() == nil
-		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+		p := Page{Books: []Book{}}
+		rows, _ := db.Query("select pk, title, author, classification from books")
+		for rows.Next() {
+			var b Book
+			rows.Scan(&b.PK, &b.Title, &b.Author, &b.Classification)
+			p.Books = append(p.Books, b)
+		}
+
+		if err = template.Execute(w, p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-	})
+	}).Methods("GET")
 
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
 		var results []SearchResult
 		var err error
 
@@ -52,26 +75,90 @@ func main() {
 		if err := encoder.Encode(results); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-	})
-	fmt.Println(http.ListenAndServe(":8080", nil))
+	}).Methods("POST")
+
+	mux.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
+		var book ClassifyBookResponse
+		var err error
+
+		if book, err = find(r.FormValue("id")); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		result, err := db.Exec("insert into books (pk, title, author, id, classification) values (?, ?, ?, ?, ?)",
+			nil, book.BookData.Title, book.BookData.Author, book.BookData.ID, book.Classification.MostPopular)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		pk, _ := result.LastInsertId()
+		b := Book{
+			PK:             int(pk),
+			Title:          book.BookData.Title,
+			Author:         book.BookData.Author,
+			Classification: book.Classification.MostPopular,
+		}
+		if err := json.NewEncoder(w).Encode(b); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}).Methods("PUT")
+
+	mux.HandleFunc("/books/{pk}", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := db.Exec("delete from books where pk = ?", gmux.Vars(r)["pk"]); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}).Methods("DELETE")
+
+	n := negroni.Classic()
+	n.Use(negroni.HandlerFunc(verifyDatabase))
+	n.UseHandler(mux)
+	n.Run(":8080")
+}
+
+type ClassifyBookResponse struct {
+	BookData struct {
+		Title  string `xml:"title,attr"`
+		Author string `xml:"author,attr"`
+		ID     string `xml:"owi,attr"`
+	} `xml:"work"`
+	Classification struct {
+		MostPopular string `xml:"sfa,attr"`
+	} `xml:"recommendations>ddc>mostPopular"`
 }
 
 type ClassifySearchResponse struct {
 	Results []SearchResult `xml:"works>work"`
 }
 
+func find(id string) (ClassifyBookResponse, error) {
+	var c ClassifyBookResponse
+	body, err := classifyAPI("http://classify.oclc.org/classify2/Classify?summary=true&owi=" + url.QueryEscape(id))
+
+	if err != nil {
+		return ClassifyBookResponse{}, err
+	}
+	err = xml.Unmarshal(body, &c)
+	return c, err
+}
+
 func search(query string) ([]SearchResult, error) {
-	var resp *http.Response
-	var err error
-	if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?summary=true&title=" + url.QueryEscape(query)); err != nil {
-		return []SearchResult{}, err
-	}
-	defer resp.Body.Close()
-	var body []byte
-	if body, err = ioutil.ReadAll(resp.Body); err != nil {
-		return []SearchResult{}, err
-	}
 	var c ClassifySearchResponse
+	body, err := classifyAPI("http://classify.oclc.org/classify2/Classify?summary=true&title=" + url.QueryEscape(query))
+	if err != nil {
+		return []SearchResult{}, err
+	}
 	err = xml.Unmarshal(body, &c)
 	return c.Results, err
+}
+
+func classifyAPI(url string) ([]byte, error) {
+	var resp *http.Response
+	var err error
+	if resp, err = http.Get(url); err != nil {
+		return []byte{}, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
 }
